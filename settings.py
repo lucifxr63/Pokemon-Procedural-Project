a20a8990b9875f0f8df757df42456f76dcbeed48
lucifxr63 <lucianoalonso2000@gmail.com>
2024-05-07 19:58:21 -0400

    a

---
+++ settings.py
@@ -0,0 +1,19 @@
+# Dimensiones de la ventana
+WIDTH, HEIGHT = 640, 480
+
+# Colores
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)  # Color para las líneas de separación de chunks
+
+# Configuraciones del jugador
+PLAYER_SIZE = 20
+VELOCITY = 5
+
+# Configuraciones del mapa
+CHUNK_SIZE = 4
+TILE_SIZE = 80
+CHUNK_PIXEL_SIZE = CHUNK_SIZE * TILE_SIZE
+
+# Cantidad inicial de chunks
+CHUNKS_HORIZONTAL = 5
+CHUNKS_VERTICAL = 3
